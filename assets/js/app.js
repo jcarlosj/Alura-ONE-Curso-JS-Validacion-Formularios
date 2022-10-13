cf2ae45a2a93ef830c6f26b185c67564ecdf9af9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-13 18:22:01 -0500

    [+] Form: Personaliza mensaje de error de acuerdo al tipo de campo

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,31 +9,89 @@ $inputs.forEach( input => {
     });
 });
 
-
-function validateDataSet( input ) {
-    const type = input.dataset.type;
-
-    console.log( input.validity );
-
-    // Valida si el tipo de campo a validar existe
-    if( fieldValidators[ type ] )
-        fieldValidators[ type ] ( input );
-
-    // Valida si el valor del campo es valido
-    if( input.validity.valid )
-        input.parentElement.classList.remove( 'input-container--invalid' );
-    else
-        input.parentElement.classList.add( 'input-container--invalid' );
-
-}
-
 const fieldValidators = {
     "input-birth": ( input ) => validateDate( input )
 }
 
+const typeErrors = [ 'valueMissing', 'typeMismatch', 'patternMismatch', 'customError' ];
+
+const errorMessages = {
+    'input-name': {
+        valueMissing: 'El campo nombre no puede estar vacio'
+    },
+    'input-email': {
+        valueMissing: 'El campo email no puede estar vacio',
+        typeMismatch: 'El correo no es válido'
+    },
+    'input-password': {
+        valueMissing: 'El campo contraseña no puede estar vacio',
+        patternMissing: 'El campo contraseña debe tener entre 6 a 12 caracteres, debe contener una letra minúscula, una mayúscula, un número, NO puede contener caracteres especiales.'
+    },
+    'input-birth': {
+        valueMissing: 'El campo fecha nacimiento no puede estar vacio',
+        customError: `Debes tener al menos ${ ageToBeOlder } años de edad`
+    },
+    'input-phone': {
+        valueMissing: 'El campo teléfono no puede estar vacio'
+    },
+    'input-address': {
+        valueMissing: 'El campo dirección no puede estar vacio'
+    },
+    'input-city': {
+        valueMissing: 'El campo ciudad no puede estar vacio'
+    },
+    'input-state': {
+        valueMissing: 'El campo estado no puede estar vacio'
+    }
+}
+
+
+function validateDataSet( input ) {
+    const typeInput = input.dataset.type;
+
+    console.log( input.validity );
+
+    // Valida si el tipo de campo a validar existe
+    if( fieldValidators[ typeInput ] )
+        fieldValidators[ typeInput ] ( input );
+
+    // Valida si el valor del campo es valido
+    if( input.validity.valid ) {
+        input.parentElement.classList.remove( 'input-container--invalid' );
+        input.parentElement.querySelector( '.input-message-error' ).innerHTML = '';
+    }
+    else {
+        input.parentElement.classList.add( 'input-container--invalid' );
+        input.parentElement.querySelector( '.input-message-error' ).innerHTML = showErrorMessage( typeInput, input );
+    }
+
+}
+
+function showErrorMessage( typeInput, input ) {
+    let message = '';
+
+    console.group( `type: ${ typeInput }` );
+
+    /** Itera todos los tipos de errores que hemos considerado */
+    typeErrors.forEach( typeError => {
+        // console.log( `${ typeError }: ${ input.validity[ typeError ] }` );                                              // Mostrará todos los tipos de errores y sus errores
+        console.log( `${ input.validity[ typeError ] ? `${ typeError }: ${ input.validity[ typeError ] }` : '' }` );    // Mostrará solo los tipos de errores que contiene el elemento
+
+        // Verifica si existe este tipo de error en el elemento input
+        if( input.validity[ typeError ] )
+            message = errorMessages[ typeInput ][ typeError ];
+
+    });
+
+    console.groupEnd();
+
+    return message;
+}
+
+
 function validateDate( input ) {
     const
-        dateValue = input.value
+        dateValue = input.value,
         message = '';
 
     console.log( `validateDate!` );
