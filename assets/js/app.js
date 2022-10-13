85f5666cc6ffb42fc4b701e37bbaa379f27daa73
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2022-10-13 17:32:46 -0500

    [+] Form: aplica/elimina clase si el valor del campo no es valido

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,34 +5,44 @@ console.log( $inputs );
 
 $inputs.forEach( input => {
     input.addEventListener( 'blur', event => {
-        validateDate( event.target );       // Valida si la fecha del usuario pertenece a un mayor de edad
+        validateDataSet( event.target );       // Valida si la fecha del usuario pertenece a un mayor de edad
     });
 });
 
 
-function validateDataSet( element ) {
-    const type = element.dataset.type;
+function validateDataSet( input ) {
+    const type = input.dataset.type;
+
+    console.log( input.validity );
 
     // Valida si el tipo de campo a validar existe
     if( fieldValidators[ type ] )
-        fieldValidators[ type ] ( element );
+        fieldValidators[ type ] ( input );
+
+    // Valida si el valor del campo es valido
+    if( input.validity.valid )
+        input.parentElement.classList.remove( 'input-container--invalid' );
+    else
+        input.parentElement.classList.add( 'input-container--invalid' );
 
 }
 
 const fieldValidators = {
-    "input-birth": ( element ) => validateDate( element )
+    "input-birth": ( input ) => validateDate( input )
 }
 
-function validateDate( inputElement ) {
+function validateDate( input ) {
     const
-        dateValue = inputElement.value
+        dateValue = input.value
         message = '';
+
+    console.log( `validateDate!` );
 
     if( isOlder( new Date( dateValue ) ) )
         message = `Debes tener al menos ${ ageToBeOlder } años de edad.`;
 
     // setCustomValidity: define el mensaje de validación personalizado para el elemento seleccionado con el mensaje especifico
-    inputElement.setCustomValidity( message );
+    input.setCustomValidity( message );
 
 }
 
